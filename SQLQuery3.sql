c0429602476f3079b06bca769725e9950a3b5fc5
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-11 00:42:01 -0700

    Adds values to the type column

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -6,3 +6,12 @@ CREATE TABLE table_animalia (
     animalia_id INT PRIMARY KEY NOT NULL IDENTITY (1,1),
     animalia_type VARCHAR(100) NOT NULL
 );
+
+INSERT INTO table_animalia
+    (animalia_type)
+    VALUES
+    ('Vertebrate'),
+    ('Invertebrate')
+;
+
+SELECT * FROM table_animalia;
