85b7de129f9cbaa476fbd17f2fa0aedd7d1ea930
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-11 00:49:36 -0700

    Adds a new table for the taxonomy class

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -4,7 +4,7 @@ USE database_zoo;
 
 CREATE TABLE table_animalia (
     animalia_id INT PRIMARY KEY NOT NULL IDENTITY (1,1),
-    animalia_type VARCHAR(100) NOT NULL
+    animalia_type VARCHAR(50) NOT NULL
 );
 
 INSERT INTO table_animalia
@@ -15,3 +15,11 @@ INSERT INTO table_animalia
 ;
 
 SELECT * FROM table_animalia;
+
+
+
+CREATE TABLE table_class (
+    class_id INT PRIMARY KEY NOT NULL IDENTITY (100, 1),
+    class_type VARCHAR(60) NOT NULL
+);
+
