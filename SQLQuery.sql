c21289b35475fd21874615ff38ba0f6436563c53
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-12 22:07:49 -0700

    Adds tables for Orders, care needs, nutrition, habitats, and keepers

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -74,4 +74,65 @@ SELECT UPPER(Class_type) FROM table_class WHERE Class_type = 'Aves';
 SELECT COUNT(Class_type) FROM table_class WHERE Class_type = 'Aves';
 
 
--- ---------------------------------------
+-- --------------------------------------------
+
+-- Create a table for an animal's Order within its taxonomy
+CREATE TABLE table_Order (
+	Order_id INT PRIMARY KEY NOT NULL IDENTITY (10, 10),
+	Order_type VARCHAR(50) NOT NULL
+);
+
+
+
+SELECT * FROM table_Order;
+
+-- --------------------------------------------
+
+-- Create a table for each animal's care needs:
+CREATE TABLE animal_care (
+	care_id VARCHAR(50) PRIMARY KEY NOT NULL,
+	care_type VARCHAR(50) NOT NULL,
+	supervisor_id INT NOT NULL
+);
+
+
+SELECT * FROM animal_care;
+
+-- --------------------------------------------
+
+-- Create a table for each animal's nutritional needs
+CREATE TABLE animal_diet (
+	nutrition_id INT PRIMARY KEY NOT NULL IDENTITY (1000, 1000),
+	nutrition_type VARCHAR(50) NOT NULL,
+	delivery_cost MONEY NOT NULL
+);
+
+
+
+SELECT * FROM animal_diet;
+
+-- --------------------------------------------
+
+-- Create a table for each animal's habitat:
+CREATE TABLE habitat (
+	habitat_id INT PRIMARY KEY NOT NULL IDENTITY (55550, 1),
+	habitat_type VARCHAR(50) NOT NULL,
+	maintenance_cost MONEY NOT NULL
+);
+
+
+SELECT * FROM animal_diet;
+
+-- --------------------------------------------
+
+-- Create a table of zookeepers for each type of animal:
+CREATE TABLE zookeepers (
+	employee_id INT PRIMARY KEY NOT NULL IDENTITY (202400000,1),
+	keeper_FirstName VARCHAR(100) NOT NULL,
+	keeper_LastName VARCHAR(100) NOT NULL,
+	keeper_Contact VARCHAR(20) NOT NULL
+);
+
+
+SELECT * FROM zookeepers;
+-- --------------------------------------------
