b050aef7a9ec30ec4eb21cf0cd7d6fba8f07e5f1
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-15 23:07:30 -0700

    Shows how to use the stored getAnimalInfo procedure

---
+++ Using_StoredQueryProcedure.sql
@@ -0,0 +1,10 @@
+USE stored_database
+
+EXEC [dbo].[getAnimalInfo] 'jaguar - Pantanal'             
+
+
+/*
+The user would type whichever animal they are searching for in between the quotation marks. This is just an example to show it using the jaguar named Pantanal.
+
+EXEC is an accepted abbreviation for EXECUTE
+*/
