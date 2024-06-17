bda35fd26defb62763ef74ce954037f0c784926f
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-17 02:16:21 -0700

    Explains schemas

---
+++ Example_Schema.sql
@@ -0,0 +1,6 @@
+-- A schema is a method to categorize and compartmentalize stored procedures.  It also provides a method for managing permissions and restrictions.
+
+/* Create a schema named Academy_HR and assign authorization to the database owner (dbo) */
+CREATE SCHEMA [Academy_HR] AUTHORIZATION [dbo]
+
+-- Schemas can be found within the Security folder of a database.
